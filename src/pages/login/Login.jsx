1b62b72c489d0ed94ee4807c1d27417a2348857c
* <taysongt@github.com>
2024-05-04 03:45:41 +0300

    k

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -30,7 +30,9 @@ const Login = () => {
       axios.post('/login', { username, password}, {withCredentials: true})
       .then(({data})=>{
         if(data.success){
-          // toast.success(data.message)
+          Cookies.set('access_token', data.token, {expires: new Date(data.expires)})
+          Cookies.set('username', data.username, {expires: new Date(data.expires)})
+          Cookies.set('user_id', data.user_id, {expires: new Date(data.expires)})
           navigate('/')
         }else{
           toast.error(data.message)
